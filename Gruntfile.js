f41c40875bbfa645874b675432bee7af21ada7fa
Rezki Bangsawan <rbangsawan@gmail.com>
2013-09-25 04:03:40 +0700

    setup Gruntfile & add matchdep, connect-livereload to node_modules

--- Gruntfile.js
+++ Gruntfile.js
@@ -3,3 +3,36 @@
  * @author Rezki Bangsawan
  * @url https://github.com/rbangsawan/forvia
  */
+
+/**
+ * Livereload and connect variables.
+ */
+var LIVERELOAD_PORT = 35729;
+var lrSnippet = require('connect-livereload')({
+	port: LIVERELOAD_PORT
+});
+var mountFolder = function (connect, dir) {
+	return connect.static(require('path').resolve(dir));
+};
+
+module.exports = function (grunt) {
+
+	'user strict';
+
+	/**
+	* Dynamically load npm tasks.
+	*/
+	require('matchdep').filterDev('grunt-*').forEach(grunt.loadNpmTasks);
+
+	// Initialize Grunt config object.
+	grunt.intiConfig({
+		// 
+	});
+
+	// Default (Dev) grunt task, call by typing "grunt" on command line.
+	grunt.registerTask('default', []);
+
+	// Production grunt task, call by typing "grunt dist" on command line.
+	grunt.registerTask('dist', []);
+
+};
